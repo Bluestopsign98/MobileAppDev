1bb63240cf2004b376ecf28c41510c54a834b626
Matthew Koser <mattkoser24@gmail.com>
2021-04-20 12:57:13 -0400

    adding settings fixed big bug

--- app/src/main/java/com/example/budgetapp/SettingsFragment.kt
+++ app/src/main/java/com/example/budgetapp/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.example.budgetapp
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+<<<<<<< Updated upstream
 import java.util.ArrayList
 
 
@@ -13,8 +16,59 @@ class SettingsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-    }
+=======
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.budgetapp.ui.home.HomeViewModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_add_entry.*
+import java.util.ArrayList
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Switch
+import kotlinx.android.synthetic.main.fragment_settings.*
 
+
+class SettingsFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+    private var incomeCategoriesList = ArrayList<String>()
+    private var expenseCategoriesList = ArrayList<String>()
+    private var income : Boolean = false
+    private var currentList = ArrayList<String>()
+    lateinit var myAdapter: ArrayAdapter<String>
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
+        retrieveCategories(root)
+        val myLV = root.findViewById<ListView>(R.id.categoryListView)
+        currentList.add("hello")
+        myAdapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, currentList)
+        myLV.adapter = myAdapter
+        updateList()
+        val ts = root.findViewById<Switch>(R.id.typeSwitch)
+        ts.setOnClickListener()
+        {
+            categorySwitcher(root)
+        }
+
+        myLV.setOnItemClickListener { list, item, position, id ->
+            //remove category
+        }
+        return root
+>>>>>>> Stashed changes
+    }
+    private fun retrieveCategories(view: View) {
+        val sharedPreferences = this.getActivity()?.getSharedPreferences("BudgetApp", AppCompatActivity.MODE_PRIVATE)
+        if (sharedPreferences!!.contains("IncomeCategories")) {
+            // --- Retrieve the income category list ---
+            val incomeCategories = sharedPreferences?.getString("IncomeCategories", "").toString()
+
+<<<<<<< Updated upstream
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,5 +86,55 @@ class SettingsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
+=======
+            // Create an instance of Gson
+            val gson = Gson()
+            // create an object expression that descends from TypeToken
+            // and then get the Java Type from that
+            val sType = object : TypeToken<ArrayList<String>>() {}.type
+            // provide the type specified above to fromJson() method
+            // this will deserialize the previously saved Json into an object of the specified type (e.g., list)
+            incomeCategoriesList = gson.fromJson<ArrayList<String>>(incomeCategories, sType)
+        }
+
+        if (sharedPreferences.contains("ExpenseCategories")) {
+
+            // --- Retrieve the expense category list ---
+            val expenseCategories = sharedPreferences.getString("ExpenseCategories", "") ?: ""
+
+            // Create an instance of Gson
+            val gson = Gson()
+            // create an object expression that descends from TypeToken
+            // and then get the Java Type from that
+            val sType = object : TypeToken<ArrayList<String>>() {}.type
+            // provide the type specified above to fromJson() method
+            // this will deserialize the previously saved Json into an object of the specified type (e.g., list)
+            expenseCategoriesList = gson.fromJson<ArrayList<String>>(expenseCategories, sType)
+        }
+        Log.d("ASDF",expenseCategoriesList.toString())
+
+    }
+
+    fun categorySwitcher(view: View){
+        // --- Set income boolean to match the switch contents ---
+        income = typeSwitch.isChecked
+        updateList()
+    }
+
+    fun updateList()
+    {
+        if(income)
+        {
+            currentList = incomeCategoriesList
+        }
+        else
+        {
+            currentList = expenseCategoriesList
+        }
+        Log.d("TAG", currentList.toString())
+        myAdapter.notifyDataSetChanged()
+    }
+
+>>>>>>> Stashed changes
 
 }
--- app/src/main/java/com/example/budgetapp/ui/EntryDetailsActivity.kt
+++ app/src/main/java/com/example/budgetapp/ui/EntryDetailsActivity.kt
@@ -12,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_add_entry.*
 private const val TAG = "DetailsActivity"
 
 class EntryDetailsActivity : AppCompatActivity() {
+<<<<<<< Updated upstream
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry_details)
@@ -31,13 +32,208 @@ class EntryDetailsActivity : AppCompatActivity() {
         amount_details_id.setText(currentTransaction.amount)
         desc_details_id.setText(currentTransaction.description)
         date_details_id.setText(currentTransaction.date)
+=======
+
+    val dbHelper = DatabaseHelper(this)
+    var categorySelected = ""
+    var income = false
+    private var transactionID = -1
+    private var incomeCategoriesList = ArrayList<String>()
+    private var expenseCategoriesList = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entry_details)
+
+        transactionID = intent.getIntExtra("TransactionID", 0)
+        Log.d(TAG, "onCreate HELLO: $transactionID")
 
 
+//         --- Cursor is used to iterate though the result of the database get call
+        val cursor = dbHelper.getDataByID(transactionID)
+
+        cursor.moveToNext()
+        var currentTransaction = Transaction(
+            cursor.getInt(0),
+            cursor.getString(1),
+            cursor.getFloat(2),
+            cursor.getString(4),
+            cursor.getString(3),
+            cursor.getString(5),
+            cursor.getInt(6) > 0,
+            R.drawable.dollar_sign_symbol
+        )
+
+        cursor.close()
+        dbHelper.close()
+
+
+        Log.d(TAG, "onCreate: $currentTransaction")
+
+        name_details_id.setText(currentTransaction.name)
+        amount_details_id.setText(currentTransaction.amount.toString())
+        desc_details_id.setText(currentTransaction.description)
+        date_details_id.setText(currentTransaction.date)
+
+        categorySelected = currentTransaction.category
+        income = currentTransaction.income
+>>>>>>> Stashed changes
+
+
+<<<<<<< Updated upstream
+=======
+        configureSpinner()
+
+        var i = 0
+        if(income){
+            while (i < incomeCategoriesList.size){
+                Log.d("HSDFSHPDO", "i is " + i)
+                if(incomeCategoriesList[i] == categorySelected){
+                    category_details_id.setSelection(i)
+                }
+                i++
+            }
+        }else{
+            while (i < expenseCategoriesList.size){
+                Log.d("HSDFSHPDO", "i is " + i)
+                if(expenseCategoriesList[i] == categorySelected){
+                    category_details_id.setSelection(i)
+                }
+                i++
+            }
+
+        }
+
+        //category_details_id.setSelection() = categorySelected
+>>>>>>> Stashed changes
 
     }
     
 
+<<<<<<< Updated upstream
 
 
+=======
+
+    fun deleteEntry(view: View) {
+
+        // --- Add new entry to database ---
+        try {
+            dbHelper.deleteData(
+                    transactionID.toString())
+
+        } catch (e: Exception) {
+            Log.e(TAG, "error: $e")
+        }
+
+        dbHelper.close()
+
+        // --- Return to Overview ---
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+    }
+
+    fun submitEntry(view: View) {
+
+        var amount = amount_details_id.text.toString().toFloat()
+        if(income){
+        }else{
+            amount *= -1
+        }
+
+        // --- Add new entry to database ---
+        try {
+            dbHelper.updateData(
+                transactionID.toString(),
+                name_details_id.text.toString(),
+                amount_details_id.text.toString().toFloat(),
+                date_details_id.text.toString(),
+                desc_details_id.text.toString(),
+                categorySelected,
+                income
+            )
+        } catch (e: Exception) {
+            Log.e(TAG, "error: $e")
+        }
+
+       dbHelper.close()
+
+        // --- Return to Overview ---
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+    }
+
+    fun cancel(view: View) {
+
+       // dbHelper.close()
+
+        // --- Return to Overview ---
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+    }
+
+
+    private fun retrieveCategories() {
+        val sharedPreferences = getSharedPreferences("BudgetApp", MODE_PRIVATE)
+        if (sharedPreferences.contains("IncomeCategories")) {
+            // --- Retrieve the income category list ---
+            val incomeCategories = sharedPreferences.getString("IncomeCategories", "").toString()
+
+            // Create an instance of Gson
+            val gson = Gson()
+            // create an object expression that descends from TypeToken
+            // and then get the Java Type from that
+            val sType = object : TypeToken<ArrayList<String>>() {}.type
+            // provide the type specified above to fromJson() method
+            // this will deserialize the previously saved Json into an object of the specified type (e.g., list)
+            incomeCategoriesList = gson.fromJson<ArrayList<String>>(incomeCategories, sType)
+        }
+
+        if (sharedPreferences.contains("ExpenseCategories")) {
+
+            // --- Retrieve the expense category list ---
+            val expenseCategories = sharedPreferences.getString("ExpenseCategories", "") ?: ""
+
+            // Create an instance of Gson
+            val gson = Gson()
+            // create an object expression that descends from TypeToken
+            // and then get the Java Type from that
+            val sType = object : TypeToken<ArrayList<String>>() {}.type
+            // provide the type specified above to fromJson() method
+            // this will deserialize the previously saved Json into an object of the specified type (e.g., list)
+            expenseCategoriesList = gson.fromJson<ArrayList<String>>(expenseCategories, sType)
+        }
+        Log.d("ASDF",expenseCategoriesList.toString())
+
+    }
+
+    fun configureSpinner(){
+        //Configure Spinner
+        val category_Spinner: Spinner = category_details_id;
+
+        if(income){
+            val adapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, incomeCategoriesList)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            category_Spinner.setAdapter(adapter)
+        }else{
+            val adapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, expenseCategoriesList)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            category_Spinner.setAdapter(adapter)
+        }
+
+        category_Spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if(income){
+                    categorySelected = incomeCategoriesList[position]
+                }else{
+                    categorySelected = expenseCategoriesList[position]
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+    }
+>>>>>>> Stashed changes
 
 }
